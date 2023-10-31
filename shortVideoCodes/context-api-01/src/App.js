ce39209f2c26035a1866d3283ec5e4a6d0d2aea7
Yashoda <yashodalakshmi999@gmail.com>
2023-10-31 22:15:57 +0530

    Context API

--- shortVideoCodes/context-api-01/src/App.js
+++ shortVideoCodes/context-api-01/src/App.js
@@ -1,29 +1,42 @@
-import React, { useState } from "react";
+import React, { Fragment } from "react";
+import Provider from "./provider";
+import Context from "./context";
+import "./App.css"
 
-const GrandChild = props => {
-  return (
-    <div>
-      <h3>Grandchild: </h3>
-      <Child brand={props.brand} />
-    </div>
-  );
+const Agents = () => {
+  return <AgentOne />;
 };
 
-const Child = props => {
+const AgentOne = () => {
+  return <AgentTwo />;
+};
+
+const AgentTwo = () => {
+  return <AgentBond />;
+};
+
+const AgentBond = () => {
   return (
-    <div>
-      <h2>Child: {props.brand}</h2>
-    </div>
+    <Context.Consumer>
+      {context => (
+        <Fragment>
+          <h3>Agent Information</h3>
+          <p>Mission Name: {context.data.mname}</p>
+          <h2>Mission Status: {context.data.accept}</h2>
+          <button onClick={context.isMissionAccepted} className="button" >Choose to accept</button>
+        </Fragment>
+      )}
+    </Context.Consumer>
   );
 };
 
 const App = () => {
-  const [brandname] = useState("Amazon");
   return (
     <div>
-      <h1>Hello</h1>
-
-      <GrandChild brand={brandname} />
+      <h1>Context API</h1>
+      <Provider>
+        <Agents />
+      </Provider>
     </div>
   );
 };
