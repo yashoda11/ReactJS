04e2309ea240ab5a34a058aec4d0cefd635a1695
Yashoda <yashodalakshmi999@gmail.com>
2023-11-17 17:25:54 +0530

    React Practise

--- liveClassCodes/19-Feb-2023/react-app/src/App.js
+++ liveClassCodes/19-Feb-2023/react-app/src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
+import Axios from "axios";
 
 function App(){
+
+    const [details, setDetails] = useState({});
+
+    const fetchDetails = async () => {
+        const {data} = await Axios.get("https://randomuser.me/api");
+        console.log("RESPONSE", data);
+
+        const details = data.results[0];
+        setDetails(details)
+    };
     
     let list = ["hitest", "Anurag", "Sahu"];
+
+    useEffect(()=>{
+        fetchDetails()
+    }, []);
+
+
     return(
         <>
         <div>App</div>
-        <Card myname="hitesh" list={list} />
+        <Card myname="hitesh" list={list} details={details} />
+        <button
+        onClick={fetchDetails}
+        >get Details
+        </button>
         </>
     )
 }
--- liveClassCodes/19-Feb-2023/react-app/src/Card.jsx
+++ liveClassCodes/19-Feb-2023/react-app/src/Card.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import "./App.css"
 
-function Card({myname, list}){
+function Card({myname, list, details}){
     // console.log(props);
     return(
         <>
         This is a Card
-        <h1>myname</h1>
+        <div>myname</div>
+        <h1>Name : {details.name?.title} {details.name?.first} {details.name?.last}</h1>
+        <h2>Gender : {details.gender}</h2>
+        <h2>Country : {details.location?.country}</h2>
+        <h2>Phone : {details.phone}</h2>
+        <h2>Email : {details.email}</h2>
+
         <ul>
             {list.map((l) => (
                 <li key={l}>{l}</li>
