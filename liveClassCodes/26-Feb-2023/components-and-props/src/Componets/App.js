3a833258fe4b17f7405812712554e799f708d86f
Yashoda <yashodalakshmi999@gmail.com>
2023-11-28 00:05:33 +0530

    REACT

---
+++ liveClassCodes/26-Feb-2023/components-and-props/src/Componets/App.js
@@ -0,0 +1,15 @@
+import Card from './Card';
+
+const App = () => {
+  return (
+    <>
+    <h1>Components and Props</h1>
+    <h3>Example : </h3>
+    <Card name="Ram"/>
+    <Card name="Shiv"/>
+    <Card name="Sai"/>
+    </>
+  );
+}
+
+export default App;
---
+++ liveClassCodes/26-Feb-2023/components-and-props/src/Componets/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = (props) => {
+  return (
+    <>
+    <p>Hello, My name is : {props.name}</p>
+    </>
+  );
+}
+
+export default Card;
