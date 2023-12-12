ba32a1df34b9420958d1142a5a84c9783173e8d3
Yashoda <yashodalakshmi999@gmail.com>
2023-12-12 13:05:49 +0530

    Firebase

--- shortVideoCodes/firebase-with-github-app-10/src/App.js
+++ shortVideoCodes/firebase-with-github-app-10/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+// bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// React Toasity
+import "react-toastify/dist/ReactToastify.min.css";
+import { ToastContainer } from "react-toastify";
+
+//react-router
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
+//firebase
+import firebase from 'firebase/app';
+import "firebase/auth";
+
+// Components
+import Home from "./Pages/Home";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import PageNotFound from "./Pages/PageNotFound";
+import { UserContext } from './Context/UserContext';
+
+const App = () => {
+  const [user, setUser] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ToastContainer />
+      <UserContext.Provider value={{user, setUser}}>
+      </UserContext.Provider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+
+    </Router>
   );
-}
+};
 
 export default App;
